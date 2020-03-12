a373579184bab474b60c0db17250ff10abeaa18f
konradsiekacz <konradsiekacz@gmail.com>
2020-03-12 20:34:44 +0100

    Added Attribute class

---
+++ Attribute.java
@@ -0,0 +1,12 @@
+public class Attribute{
+    
+    private float value;
+
+    Attribute(float value){
+        this.value = value;   
+    }
+
+    public float getValue(){
+        return value;
+    }
+}
