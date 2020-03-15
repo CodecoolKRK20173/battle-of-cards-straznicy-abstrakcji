a7d2fa5775608547568487e750886d0f26aade64
Rafal <rpajdak@gmail.com>
2020-03-15 17:41:51 +0100

    Added numberOfCopies Comparator

---
+++ NumberOfCopiesComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class NumberOfCopiesComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card firstCard, Card secondCard) {
+        return Integer.compare(firstCard.getNumberOfCopies(), secondCard.getNumberOfCopies());
+    }
+}
