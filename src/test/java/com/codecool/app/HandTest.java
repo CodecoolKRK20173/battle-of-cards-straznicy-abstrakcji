f6d0d7359c5abe552456b17d072e0e8a7c7c53cb
Rafal Pajdak <rpajdak@gmail.com>
2020-03-22 20:30:34 +0100

    Did some refactor

---
+++ src/test/java/com/codecool/app/HandTest.java
@@ -0,0 +1,13 @@
+package com.codecool.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HandTest {
+
+    @Test
+    public void passCard() {
+        assertTrue(3>1);
+    }
+}
